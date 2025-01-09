184ca8cf1197f5e89d2e3e92b21ecc945c4e8980
Marcin <mprzyby2@gmail.com>
2025-01-09 22:16:41 +0100

    highlight next line with s-v, support full page down/up

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,8 +6,10 @@ vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { noremap = true, desc = "tu
 vim.keymap.set('n', '<leader>z', ':set wrap!<CR>', { noremap = true, desc = "toggles wrapping the long lines" })
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, desc = "moves down and centers" }) 
+vim.keymap.set('n', '<C-f>', '<C-f>zz', { noremap = true, desc = "moves down and centers" }) 
 vim.keymap.set('n', '<leader>d', '<C-d>zz', { noremap = true, desc = "moves down and centers" }) 
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, desc = "move up and center" })
+vim.keymap.set('n', '<C-b>', '<C-b>zz', { noremap = true, desc = "move up and center" })
 vim.keymap.set('n', '<leader>u', '<C-u>zz', { noremap = true, desc = "move up and center" }) 
 
 vim.keymap.set('n', '<leader>b', ':Telescope buffers<CR>', { noremap = true, desc = "telescope buffers" })
@@ -15,7 +17,7 @@ vim.keymap.set('n', '<leader>b', ':Telescope buffers<CR>', { noremap = true, des
 -- Nawigacja
 vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true, desc = "next buffer" })
 vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { noremap = true, desc = "previous buffer" })
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true,, desc = "delete the buffer" })
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true, desc = "delete the buffer" })
 
 -- In config/keymaps.lua
 vim.keymap.set("v", ">", ">gv", { noremap = true, desc = "Indent and keep selection" })
@@ -32,6 +34,9 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, desc = "Move 
 -- Normal and Visual mode: H and L for start/end of line
 vim.keymap.set({ "n", "v" }, "L", "$", { noremap = true, desc = "Go to end of line" })
 vim.keymap.set({ "n", "v" }, "H", "^", { noremap = true, desc = "Go to first non-blank character" })
+
+-- s-v zaznacza linie nawet w trybie visual
+vim.keymap.set('x', '<S-v>', 'j', { noremap = true })  -- w trybie visual zaznaczenie kolejnej linii
 
 -- Operator pending mode for commands like y, d, c
 vim.keymap.set("o", "L", "$", { noremap = true, desc = "Operate to end of line" })
